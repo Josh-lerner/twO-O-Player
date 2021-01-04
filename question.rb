244b8d157b59366893e1684f5b577a27698e3577
Josh <josh.lerner95@gmail.com>
2021-01-04 18:27:05 +0000

    Create Question class

---
+++ question.rb
@@ -0,0 +1,16 @@
+class Question
+  attr_accessor :num1. :num2
+
+
+  def self.num1 
+    @num1 = rand(50)
+  end 
+
+  def self.num2
+    @num2 = rand(50)
+  end
+
+  def self.validate(input)
+    input == (@num1 + @num2)
+  end 
+end 
